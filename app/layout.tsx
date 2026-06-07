1a1ef70850115e93b0c0e887618d762ba99d9b18
017RAFIRAMDHANI <rafiramdhani1122@gmail.com>
2026-06-07 11:10:32 +0700

    responsiveNavbarMobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
         <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script> 
          {/* /* Google Font */}
